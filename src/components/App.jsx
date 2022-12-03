edb641b695a80c487ae312043752c8f3b90574d5
Oleg Bukovskiy <olbukovskiy@gmail.com>
2022-12-03 21:06:27 +0200

    add main files and folders

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
+
+
+// 30885515-e5cd8644896c6a7d3960ad51e
